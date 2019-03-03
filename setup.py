6ced3023a07ac9408f2c06fdd093099ad2ea7df4
Adriano Sokax <asosam91gmail.com>
2019-03-03 16:55:48 -0600

    se agrega setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name="Grafos",  # Nombre
+      version="0.1",  # Versión de desarrollo
+      description="Paquete para Grafos",  # Descripción del funcionamiento
+      author="Adriano Sokax",  # Nombre del autor
+      author_email='asosam91@gmail.com',  # Email del autor
+ #     license="GPL",  # Licencia: MIT, GPL, GPL 2.0...
+ #     url="http://ejemplo.com",  # Página oficial (si la hay)
+      packages=find_packages(),
+)
