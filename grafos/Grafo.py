d4fc1302f7017d4e523bf63ebb43a081c63c4b93
Adriano Sokax <asosam91gmail.com>
2019-03-03 21:06:00 -0600

    se agregan clase Gaafo

---
+++ grafos/Grafo.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Grafo:
+	def __init__(self, nodos):
+		self.__adyacencia = np.zeros((nodos,nodos))
+		#self.__n = nodos
+
+	def addArco(self, nodo1, nodo2):
+		self.__adyacencia[nodo1][nodo2] = 1
+		self.__adyacencia[nodo2][nodo1] = 1
+
+	def removeArco(self, nodo1, nodo2):
+		self.__adyacencia[nodo1][nodo2] = 0
+		self.__adyacencia[nodo2][nodo1] = 0
+
+	def __str__(self):
+		return self.__adyacencia
