0699f9826cb9fd1682cf090d99a5a1f36f0bd791
Adriano Sokax <asosam91gmail.com>
2019-03-03 20:53:52 -0600

    se modifica setup

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 from setuptools import find_packages
-#import grafos
-#from . import *
+
 
 setup(name="Libreria IA",  # Nombre
       version="0.1",  # Versión de desarrollo
